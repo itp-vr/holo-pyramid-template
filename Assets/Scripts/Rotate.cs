32ed1674639586f7df9b046bc72d067a4b89b98a
Chris Kairalla <chris@mailtochris.com>
2015-08-16 16:03:08 -0400

    initial commit

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	public float rotX = 0.1f;
+	public float rotY = 0.15f;
+	public float rotZ = 2f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(rotX, rotY, rotZ);
+	}
+}
